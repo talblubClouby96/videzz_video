2b593727d847dd99be55c7f9211a7546def01065
jjaramillo1964 <104590579+jjaramillo1964@users.noreply.github.com>
2025-03-02 23:03:19 +0700

    Update videzz_video.py

--- videzz_video.py
+++ videzz_video.py
@@ -16,7 +16,6 @@ import multiprocessing
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import google_colab_selenium as gs
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
